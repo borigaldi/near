2cfc34781d9c1e35eb7de6031efcc38c7c7e06e5
Boris Kov <bkovesh@gmail.com>
2022-02-26 16:44:30 +0300

    Fix buffer error

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+import { Buffer } from 'buffer';
+window.Buffer = window.Buffer || Buffer;
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
